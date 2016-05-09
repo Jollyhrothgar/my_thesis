fbeb035b7f4f4a02dcde375f56642b7f86f64391
Jollyhrothgar <michael.beaumier@gmail.com>
2016-05-09 14:08:41 -0700

    reorganized thesis structure to something more sane

--- open_thesis.sh
+++ open_thesis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Open everything in one place
-vim Abstract/abstract.tex Acknowlegements/acknowlegements.tex Chapter*/*.tex thesis/thesis.tex thesis/mendeley.bib thesis/packages.tex Appendix/appendix.tex
+vim sections/*.tex
 
